3bdf70c413f98f9c0276b23448ba2b3ae834115f
Ibrahim <imimazim7@gmail.com>
2022-12-16 22:15:44 +0200

    feat: created the server & port listener

---
+++ src/server/ports.js
@@ -0,0 +1,46 @@
+const {SerialPort} = require('serialport')
+const {ReadlineParser} = require('@serialport/parser-readline')
+const express = require('express')
+const cors = require('cors')
+
+// Create the application
+const app = express()
+const portNum = 9000
+
+// app.use('/', express.static('public'));
+//  CORS Security
+app.use(cors())
+
+let arduinoData = 0;
+
+// -----------------  Server End Points ----------------- //
+app.get('/', (req, res) => {
+    res.send({"data": arduinoData})
+});
+
+
+app.listen(portNum, () => {
+    console.log(`Listening at http://localhost:${portNum}`)
+})
+
+
+// -----------------  Arduino Connection Setup ----------------- //
+
+// Create a port
+const port = new SerialPort({
+    path: 'COM9',
+    baudRate: 9600,
+})
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+
+port.on('open', function () {
+    console.log('Connected Successfully')
+})
+
+// Switches the port into "flowing mode"
+parser.on('data', function (serialPortData) {
+    arduinoData = serialPortData
+    console.log(serialPortData)
+})
