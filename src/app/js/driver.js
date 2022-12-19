ffeb8b2aa27dbe44877ccf5528d8fb2c3853c227
Ibrahim <imimazim7@gmail.com>
2022-12-19 23:25:43 +0200

    feat: error message created

--- src/app/js/driver.js
+++ src/app/js/driver.js
@@ -1,17 +1,47 @@
 // -----------------------  Variables Declarations   ----------------------- //
 
-let temperatureCardValue = document.getElementById('temp')
-let phCardValue = document.getElementById('ph')
-let flowRateCardValue = document.getElementById('flowrate')
-let speedCardValue = document.getElementById('speed')
+// HTML Paramters Value
+let temperatureCardValue = document.getElementById('temp'),
+    phCardValue = document.getElementById('ph'),
+    bloodFlowRateCardValue = document.getElementById('flow-rate'),
+    levelCardValue = document.getElementById('level'),
+    pressureCardValue = document.getElementById('pressure'),
+    HCO3CardValue = document.getElementById('hco3-conc'),
+    leakageCardValue = document.getElementById('leak'),
+    dialsateFlowRateCardValue = document.getElementById('dFlowRate')
+
+// Card of Dialsyate Level Settings
+let cardOfLevel = document.getElementById('levelCard'),
+    levelCardTitle = document.getElementById('levelCardTitle'),
+    backgroundColor = cardOfLevel.style.backgroundColor,
+    defaultColor  = levelCardTitle.style.color
+
+// audio file to be played
+let audio = new Audio('../assets/mixkit-facility-alarm-908.mp3')
+
+// error message vars
+let error,
+    errorCount = 0;
+
+// Parameters data ranges
+let pressureData = [100,200,300,400],
+    bloodFlowRateData = [200,250,300,350,400,450],
+    dialsateFlowRateData = [500,600,700,800],
+    phData = [7.3,7.34,7.4,7.44],
+    changeTimeCounter = 0;
+
+// At the application start up -> error = false
+window.onload = __ => {
+    error = false
+}
 
 // -----------------------  get the data from the serial port (Server)  ----------------------- //
 
-// Variable to store the response
-
+// Variables to store the response data
 let responseTemp = 0,
     responseDistance = 0;
-function getArduinoData(){
+
+function getArduinoData() {
     // Make the request
     $.ajax({
         method: 'POST',
@@ -25,22 +55,87 @@ function getArduinoData(){
         }
     });
 
-    // Update the card values
+    // Update the measured parameters card values
     temperatureCardValue.innerHTML = responseTemp + " °C"
-    phCardValue.innerHTML =  responseDistance + " m"
-    // flowRateCardValue.innerHTML = (Math.round(((3/2)*(7/5)*(response/20))*100)/100).toString() + " m/s"
-    // speedCardValue.innerHTML = (Math.round(((7/4)*(3/5)*(response/20))*100)/100).toString() + " m/s"
+    levelCardValue.innerHTML = responseDistance + " cm"
+
+    // Update other card values sync with time
+    if (changeTimeCounter === 20){
+        phCardValue.innerHTML = (phData[getRandomInt(phData.length)]).toString()
+        dialsateFlowRateCardValue.innerHTML = dialsateFlowRateData[getRandomInt(dialsateFlowRateData.length)] + " mL/s"
+        bloodFlowRateCardValue.innerHTML = bloodFlowRateData[getRandomInt(bloodFlowRateData.length)] + " mL/s"
+        pressureCardValue.innerHTML = pressureData[getRandomInt(pressureData.length)] + " mmHg"
+        changeTimeCounter = 0
+    }
+
+    changeTimeCounter++
+}
+
+function rePlotData(){
+    Plotly.extendTraces('chartjs-dashboard-line', {y: [[getData()]]}, [0]);
+    cnt++;
+    if (cnt > 20) {
+        Plotly.relayout('chartjs-dashboard-line', {
+            xaxis: {
+                range: [cnt - 20, cnt],
+                showgrid: false
+            },
+        });
+    }
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 // Function that executes getArduinoData function every 1 second
 setInterval(function () {
     getArduinoData()
+    rePlotData()
+
+    error = responseDistance > 60;
+
+    if (error) {
+        if (errorCount === 0){
+            Swal.fire({
+                icon: 'warning',
+                text: 'Dialysate need to be refiled',
+            })
+            errorCount = 1
+        }
+
+        if (backgroundColor === 'white') {
+            cardOfLevel.style.backgroundColor = 'red'
+            backgroundColor = 'red'
+            levelCardValue.style.color = 'white'
+            levelCardTitle.style.color = 'white'
+        } else {
+            cardOfLevel.style.backgroundColor = 'white'
+            backgroundColor = 'white'
+            levelCardValue.style.color = 'black'
+            levelCardTitle.style.color = 'black'
+        }
+
+        audio.play();
+        audio.loop = true;
+        sleep(450)
+    }
+
 }, 150);
 
 
 // ----------------------- Ploting Values ----------------------- //
 
 function getData() {
+    // return Math.random()
     return responseDistance;
 }
 
@@ -72,16 +167,4 @@ Plotly.newPlot(
 
 let cnt = 0;
 
-setInterval(function(){
 
-    Plotly.extendTraces('chartjs-dashboard-line',{ y:[[getData()]]}, [0]);
-    cnt++;
-    if(cnt > 20) {
-        Plotly.relayout('chartjs-dashboard-line',{
-            xaxis:{
-                range: [cnt-20, cnt],
-                showgrid: false
-            },
-        });
-    }
-},150);
--- src/server/ports.js
+++ src/server/ports.js
@@ -9,14 +9,15 @@ const portNum = 9000
 
 
 // app.use('/', express.static('public'));
-//  CORS Security
+// CORS Security
 app.use(cors())
 
 let temperatureData = 0;
 let ultrasoundData = 0;
 let counter = 0;
 
-// -----------------  Server End Points ----------------- //
+// --------------------  Server End Points -------------------- //
+
 app.post('/', (req, res) => {
     res.send({
         body: {
@@ -36,7 +37,7 @@ app.listen(portNum, () => {
 
 // Create a port
 const port = new SerialPort({
-    path: 'COM9',
+    path: 'COM5',
     baudRate: 9600,
 })
 
@@ -49,12 +50,16 @@ port.on('open', function () {
 
 // Switches the port into "flowing mode"
 parser.on('data', function (serialPortData) {
+
+    // toggle input capture by using counter var
     if (counter === 0){
-        temperatureData = serialPortData
+        ultrasoundData = serialPortData
         counter++
     }
     else if(counter === 1){
-        ultrasoundData = serialPortData
+        temperatureData = serialPortData
         counter = 0
     }
+
+    console.log(serialPortData)
 })
